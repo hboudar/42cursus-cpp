f4438914b70a97692fea6576dcff76aa2058f06c
Cloneg7 <boudarhamza12@gmail.com>
2024-10-10 17:18:15 +0100

    starting cpp00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 15:57:38 by hboudar           #+#    #+#             */
+/*   Updated: 2024/10/10 17:14:07 by hboudar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc != 1)
+    {
+        while (argv[1] != NULL)
+        {
+            i = 0;
+            while (argv[1][i] != '\0')
+            {
+                std::cout << (char)toupper(argv[1][i]);
+                i++;
+            }
+            (argv[2] != NULL) && (std::cout << " ");
+            argv++;
+        }
+        std::cout << "\n";
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    return (0);
+}

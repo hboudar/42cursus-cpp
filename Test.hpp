7614f4a1f45a209269b3aaeed0853490616bb633
Cloneg7 <boudarhamza12@gmail.com>
2024-10-19 22:43:02 +0100

    learning about class

---
+++ Test.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Test.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudar <hboudar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 10:48:30 by hboudar           #+#    #+#             */
+/*   Updated: 2024/10/17 18:15:47 by hboudar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_HPP
+#define TEST_HPP
+
+#include <iostream>
+
+
+class Test {
+    
+public:
+
+    int foo;
+
+    Test();
+    ~Test();
+
+    void  bar(void);
+};
+
+#endif
+
+
+//deri
+//not a demon-slayer or kiqad siff
+//died
+//hashira blood related
+//kho hashira
